78a07fa82ecd125dfa6c45fdd23a486fcbcc3183
Vincent Edwards <mister.man489@gmail.com>
2025-03-15 08:56:53 -0700

    Move code to maximize h within error to a function

--- scripts/04_a.py
+++ scripts/04_a.py
@@ -10,15 +10,10 @@ y_0 = 0.05
 
 y_true = np.asin(np.sin(0.05) * np.exp(2))
 threshold = 0.01
-N = 1
 
-while True:
-    h = (x_f - x_0) / N
-    x, y = euler_method.calculate(x_0, x_f, y_0, h, f)
-    y_estimate = y[-1]
-    relative_error = np.abs(y_estimate - y_true) / y_true
-    if relative_error < threshold:
-        break
-    N += 1
+h, y_estimate, relative_error = euler_method.maximize_h_within_error(x_0, x_f, y_0, f, y_true, threshold)
 
-print(f"With h = {h:.3}, y(4) is estimated to be {y_estimate:.4} ({relative_error:.4%} from the true value)")
+if h is None:
+    print("Estimates do not get close enough to the true value as h decreases")
+else:
+    print(f"With h = {h:.3}, y(4) is estimated to be {y_estimate:.4} ({relative_error:.4%} from the true value)")
--- scripts/euler_method.py
+++ scripts/euler_method.py
@@ -11,3 +11,17 @@ def calculate(x_0, x_f, y_0, h, diff):
         y[i + 1] = y[i] + h * diff(x[i], y[i])
 
     return x, y
+
+def relative_error(x, x_true):
+    return np.abs(x - x_true) / x_true
+
+def maximize_h_within_error(x_0, x_f, y_0, diff, y_true, error_threshold, h_min=1e-9):
+    N = 1
+    while (h := (x_f - x_0) / N) > h_min:
+        x, y = calculate(x_0, x_f, y_0, h, diff)
+        y_estimate = y[-1]
+        error = relative_error(y_estimate, y_true)
+        if error < error_threshold:
+            return h, y_estimate, error
+        N += 1
+    return None, None, None
