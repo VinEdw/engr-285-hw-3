250486d08e95ed57ef91136a222d2996791a680d
Vincent Edwards <mister.man489@gmail.com>
2025-03-19 16:26:18 -0700

    Add comments to describe the calculate function

--- scripts/euler_method.py
+++ scripts/euler_method.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 def calculate(x_0, x_f, y_0, h, diff):
+    """
+    Calculate the x & y values using the simple/vector Euler method on a 1st order ODE
+
+    - x_0: starting x value
+    - x_f: final x value
+    - y_0: starting y value
+    - h: step size (this will be the rounded to be an even multiple of x_f - x_0)
+    - diff: function that takes x and y as arguments and returns dy/dx
+
+    Note: If using the vector Euler Method, treat y like the vector u
+    """
     delta_x = x_f - x_0
     N = np.round(delta_x / h).astype(int) + 1
 
